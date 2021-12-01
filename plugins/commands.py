669d314e129a94d4773b9c42231c03fb7d53d058
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-01 12:55:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,8 +9,4 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
-    await update.reply_text(
-        text=Script.START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=Script.START_BUTTONS
-    )
+    await AddUser(bot, update)
