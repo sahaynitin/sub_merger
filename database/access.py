f7a8d13e8cd4037eb05eae31ccc94a8f192b6f93
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-01 12:52:16 +0530

    Update access.py

--- database/access.py
+++ database/access.py
@@ -1,4 +1,4 @@
 from sample_config import Config
 from database.database import Database
 
-clinton = Database(Config.DATABASE_URL, Config.SESSION_NAME)
+tellybots = Database(Config.DATABASE_URL, Config.SESSION_NAME)
